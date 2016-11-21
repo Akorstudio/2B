69cc61a92f655fe05e83cc981e10d0c5e35c2cab
Akorstudio <Akorstudio@gmail.com>
2016-11-21 16:14:09 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,44 +1,48 @@
-
 import express from 'express';
 import cors from 'cors';
 
 const app = express();
 app.use(cors());
 
-app.get('/mytask2B', (req,res) => {
-  const name = (req.query.fullname.trim() || 0);
-  console.log(name);
-  if ((name === 0)|| (/[\d_\/]/.test(name))){
-      res.send("Invalid fullname");
-  }
-  else {
-    let shortName = name.split(/ +/);
-    shortName.forEach((item, i, shortName) => {
-      shortName[i] = item[0].toUpperCase() + item.slice(1).toLowerCase();
-    });
-    switch (shortName.length) {
-      case 1:
-        res.send(shortName[0]);
-        break;
-      case 2:
-        res.send(shortName[1] + " " + shortName[0][0] + ".");
-        break;
-      case 3:
-        res.send(shortName[2] + " " + shortName[0][0] + ". " + shortName[1][0] + ". ");
-        break;
-      default:
-        res.send("Invalid fullname");
-    }
-  }
+app.get('/task2A', async (req, res) => {
+	const sum = (+req.query.a || 0 ) + (+req.query.b || 0);
+	res.send(sum.toString());
 });
 
+app.get('/task2B', async (req, res) => {
 
-/*
-app.get('/mytask2A', (req, res) => {
-  const sum = (+req.query.a || 0) + (+req.query.b || 0);
-  res.send(sum.toString());
-});*/
+try {
+
+	let fullName = fullname.toLowerCase(req.query.fullnamem.trim());
+	conssole.log(fullname);
+	//fullname = fullname.toLowerCase();
+	let arr = fullname.split(/[\s]+/);
+	let result = '';
+
+	if ( (!arr) || (arr=='') || (/[0-9\d_\/]/.test(arr)) ) res.send('Invalid fullname');
+
+	if (arr.length <= 0 || arr.length > 3) {	res.send('Invalid fullname'); }
+
+		else if (arr.length === 3) {	result = arr[2].charAt(0).toUpperCase() + arr[2].substr(1) + ' '
+        + arr[0].charAt(0).toUpperCase() + '. ' + arr[1].charAt(0).toUpperCase() + '.';
+  } else if (arr.length === 2) {
+  	result = arr[1].charAt(0).toUpperCase() + arr[1].substr(1) + ' '
+        + arr[0].charAt(0).toUpperCase() + '.';
+  } else if (arr.length === 1) {
+  	result = arr[0].charAt(0).toUpperCase() + arr[0].substr(1);
+  }
+  res.send(result);
+
+
+	} catch (err) {
+			console.log(err);
+			return res.json({
+				err
+		})
+	}
+
+	});
 
 app.listen(3000, () => {
-  console.log('Your app listening on port 3000!');
+	console.log('Example app listening on port 3000!')
 });
